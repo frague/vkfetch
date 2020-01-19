23d3592f0027cea1a015ca6b18a711b4ee88affb
Nick Bogdanov <nikolay.bogdanov@gmail.com>
2020-01-20 01:38:09 +0400

    Makes combining by chunks portions

--- fetch_stream.js
+++ fetch_stream.js
@@ -49,11 +49,39 @@ function concatPieces(pieces, fileName) {
   fs.writeFileSync('playlist.txt', pieces.join('\n'));
   return executeShell('ffmpeg -f concat -safe 0 -i "playlist.txt" -c copy -y "temp.mp3"')
     .then(() => {
-      throw;
       fs.unlinkSync('playlist.txt');
-      fs.unlinkSync('chunk.mp3');
+      executeShell('rm chunk*.mp3');
       fs.renameSync('temp.mp3', fileName);
     });
+}
+
+function parseChunks(base, source) {
+  console.log('Parsing the playlist...')
+
+  return source
+    .split('\n')
+    .reduce((result, line) => {
+      if (line.includes('#EXT-X-KEY:METHOD')) {
+        key = line;
+      } else if (line.includes('#EXTINF:')) {
+        chunk = `${line}\n`;
+      } else if (line.includes('.ts?')) {
+        chunk += `${base}${line}`;
+        result.push(['' + chunk, '' + key]);
+
+        chunk = '';
+        key = '';
+      }
+      return result;
+    }, []);
+}
+
+function progress(total, index) {
+  let size = 40;
+  let percent = total / size;
+  let done = Math.ceil(index / percent);
+  let left = Math.floor((total - index) / percent);
+  return `[${'▓'.repeat(done)}${'░'.repeat(left)}]`;
 }
 
 function main(url, title) {
@@ -76,23 +104,7 @@ function main(url, title) {
 
           let [base, ] = url.split('index.m3u8', 2);
 
-          console.log('Parsing the playlist...')
-
-          let lines = data.split('\n');
-          let chunks = lines.reduce((result, line) => {
-            if (line.includes('#EXT-X-KEY:METHOD')) {
-              key = line;
-            } else if (line.includes('#EXTINF:')) {
-              chunk = `${line}\n`;
-            } else if (line.includes('.ts?')) {
-              chunk += `${base}${line}`;
-              result.push(['' + chunk, '' + key]);
-
-              chunk = '';
-              key = '';
-            }
-            return result;
-          }, []);
+          let chunks = parseChunks(base, data);
 
           let l = chunks.length;
           console.log(`${l} chunks found. Fetching:`);
@@ -104,15 +116,14 @@ function main(url, title) {
           for (let index = 0; index < l; index++) {
             let [chunk, key] = chunks[index];
 
-            console.log(`Fetching chunk ${index+1} of ${l}:`);
+            name = `chunk${index}.mp3`;
             await fetchChunk(makePlaylist(chunk, key), name);
+            process.stdout.write(`Chunk ${index+1} of ${l} fetched: ` + progress(l, index) + '      \r');
 
             pieces.push(`file '${name}'`);
-            if (!index) {
+            if (index % 30 === 0 || index == l - 1) {
               await concatPieces(pieces, resultName);
               pieces = [`file '${resultName}'`];
-            } else {
-              name = 'chunk.mp3';
             }
           }
         });
